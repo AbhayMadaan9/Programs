c76c8ee93231d7170aeb8f8dab1555ad4d2646f3
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-12-17 20:08:56 +0530

    done for today

---
+++ detect_cycle.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h> 
+/****************************************************************/
+
+    //Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+/*****************************************************************/
+//Approch1 to use unorderedset if any node is already present then we say cycle exist 
+//Approch2 to use slow, fast pointers if slow and fast pointer are same then there exist a cycle.
+//https://www.codingninjas.com/codestudio/problems/cycle-detection-in-a-singly-linked-list_628974?leftPanelTab=1
+bool detectCycle(Node *head)
+{
+	Node*slow = head;
+    Node*fast = head;
+    while(fast and fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) return true;
+    }
+    return false;
+}
+int main()
+{
+    return 0;
+}
